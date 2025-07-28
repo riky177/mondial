3eb2f9691181fcc7949cd3e60706dfce39c73c8b
riky177 <rikyandreas177@gmail.com>
2025-07-28 12:55:04 +0700

    refactor: progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'react-datepicker/dist/react-datepicker.css';
-import 'nprogress/nprogress.css';
 
 import type { Metadata } from 'next';
 
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,8 +1,7 @@
 'use client';
 
+import { ProgressProvider } from '@bprogress/next/app';
 import { SessionProvider } from 'next-auth/react';
-
-import ProgressBar from './progress-bar';
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -11,8 +10,13 @@ interface ProvidersProps {
 export default function Providers({ children }: ProvidersProps) {
   return (
     <SessionProvider>
-      <ProgressBar />
       {children}
+      <ProgressProvider
+        height="2px"
+        color="var(--color-primary)"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
     </SessionProvider>
   );
 }
